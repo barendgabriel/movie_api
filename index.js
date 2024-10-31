474bcb413f481ab495f52c0a6a238e07b25304b3
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-10-31 15:15:08 +0100

    Updated /movies route to include 10 movie entries and enhanced documentation.html with API endpoint details

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ app.get('/movies', (req, res) => {
     { title: 'Movie 1', year: 2021 },
     { title: 'Movie 2', year: 2020 },
     { title: 'Movie 3', year: 2019 },
-    // Add your top 10 movies here
+    { title: 'Movie 4', year: 2018 },
+    { title: 'Movie 5', year: 2017 },
+    { title: 'Movie 6', year: 2016 },
+    { title: 'Movie 7', year: 2015 },
+    { title: 'Movie 8', year: 2014 },
+    { title: 'Movie 9', year: 2013 },
+    { title: 'Movie 10', year: 2012 },
   ]);
 });
 
