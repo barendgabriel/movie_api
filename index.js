806a59272bd958899d736e29e9d007196cc10c15
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-11-20 20:38:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(morgan('common'));
 // Use JSON parsing for POST/PUT requests
 app.use(express.json());
 
-// Connect to MongoDB database (update to 'myFlixDB')
+// Correct MongoDB connection string (using port 27017, not 3000)
 mongoose
   .connect('mongodb://localhost:27017/myFlixDB', {
     useNewUrlParser: true,
@@ -61,47 +61,6 @@ app.get('/movies/:title', (req, res) => {
     });
 });
 
-// GET /genres/:name - Returns data about a genre by name
-app.get('/genres/:name', (req, res) => {
-  const { name } = req.params;
-  Movies.find({ 'Genre.Name': name }) // Find movies by genre name
-    .then((movies) => {
-      if (movies.length > 0) {
-        const genre = movies[0].Genre; // Use the first movie's genre
-        res.json({ genreName: name, description: genre.Description });
-      } else {
-        res.status(404).json({ error: 'Genre not found' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ error: 'Error retrieving genre' });
-    });
-});
-
-// GET /directors/:name - Returns data about a director by name
-app.get('/directors/:name', (req, res) => {
-  const { name } = req.params;
-  Movies.find({ 'Director.Name': name }) // Find movies by director's name
-    .then((movies) => {
-      if (movies.length > 0) {
-        const director = movies[0].Director; // Use the first movie's director details
-        res.json({
-          name: director.Name,
-          bio: director.Bio,
-          birthYear: director.birthYear,
-          deathYear: director.deathYear,
-        });
-      } else {
-        res.status(404).json({ error: 'Director not found' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ error: 'Error retrieving director' });
-    });
-});
-
 // POST /users - Allows a new user to register
 app.post('/users', (req, res) => {
   const { username, password, email, birthday } = req.body;
@@ -120,80 +79,6 @@ app.post('/users', (req, res) => {
   } else {
     res.status(400).json({ error: 'Please provide all required fields' });
   }
-});
-
-// PUT /users/:username - Updates user information (username, password, email, birthday)
-app.put('/users/:username', (req, res) => {
-  const { username } = req.params;
-  const { password, email, birthday } = req.body;
-
-  Users.findOneAndUpdate(
-    { Username: username },
-    { $set: { Password: password, Email: email, Birthday: birthday } },
-    { new: true } // Return the updated user
-  )
-    .then((user) => {
-      if (user) {
-        res.json({ message: 'User updated successfully', user });
-      } else {
-        res.status(404).json({ error: 'User not found' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ error: 'Error updating user' });
-    });
-});
-
-// PUT /users/:username/favorites - Adds/removes movie from user's favorites
-app.put('/users/:username/favorites', (req, res) => {
-  const { username } = req.params;
-  const { movieId, action } = req.body; // action should be 'add' or 'remove'
-
-  Users.findOne({ Username: username })
-    .then((user) => {
-      if (user) {
-        if (action === 'add') {
-          user.FavoriteMovies.push(movieId); // Add movieId to favorites
-        } else if (action === 'remove') {
-          user.FavoriteMovies.pull(movieId); // Remove movieId from favorites
-        } else {
-          return res.status(400).json({ error: 'Invalid action' });
-        }
-
-        user
-          .save() // Save the updated user
-          .then(() => res.json({ message: 'Favorites updated', user }))
-          .catch((err) => {
-            console.error(err);
-            res.status(500).json({ error: 'Error updating favorites' });
-          });
-      } else {
-        res.status(404).json({ error: 'User not found' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ error: 'Error retrieving user' });
-    });
-});
-
-// DELETE /users/:username - Deregisters a user (deletes user from database)
-app.delete('/users/:username', (req, res) => {
-  const { username } = req.params;
-
-  Users.findOneAndDelete({ Username: username }) // Find and delete user
-    .then((user) => {
-      if (user) {
-        res.json({ message: 'User deregistered successfully', user });
-      } else {
-        res.status(404).json({ error: 'User not found' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ error: 'Error deregistering user' });
-    });
 });
 
 // Serve documentation.html file from the public folder
