c416016e3868dfbe53aabd971cc23b21587835e5
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-11-20 18:30:08 +0100

    Updated models.js

--- models.js
+++ models.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose'); // Import mongoose
+
+// Define the movie schema
 let movieSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
@@ -13,3 +16,20 @@ let movieSchema = mongoose.Schema({
   imageURL: String,
   featured: Boolean,
 });
+
+// Define the user schema
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+});
+
+// Create the Movie and User models
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+// Export the models for use in other files
+module.exports.Movie = Movie;
+module.exports.User = User;
