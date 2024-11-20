a7e721ed827fa7975c67bd4185d2eb57e0ad3c5a
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-11-20 14:40:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -92,3 +92,12 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost:27017/movieAPI', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
