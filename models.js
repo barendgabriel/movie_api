0cb461371b14c875dbf1bfd8c645050f3deef141
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-11-20 18:15:41 +0100

    Update models.js

--- models.js
+++ models.js
@@ -1,30 +1,15 @@
-const mongoose = require('mongoose');
 let movieSchema = mongoose.Schema({
-  Title: { type: String, required: true },
-  Description: { type: String, required: true },
-  Genre: {
-    Name: String,
-    Description: String,
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: {
+    name: String,
+    description: String,
   },
-  Director: {
-    Name: String,
-    Bio: String,
+  director: {
+    name: String,
+    bio: String,
   },
-  Actors: [String],
-  ImagePath: String,
-  Featured: Boolean,
+  actors: [String],
+  imageURL: String,
+  featured: Boolean,
 });
-
-let userSchema = mongoose.Schema({
-  Username: { type: String, required: true },
-  Password: { type: String, required: true },
-  Email: { type: String, required: true },
-  Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-});
-
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
-
-module.exports.Movie = Movie;
-module.exports.User = User;
