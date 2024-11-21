874c739c01a505a79ecee36eb4d32373f160ecf9
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-11-21 12:32:07 +0100

    Update index.js
    Update and expand endpoints

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Models = require('./models.js'); // Import Mongoose models
 
 const Movies = Models.Movie; // Assign the Movie model to a constant for easy access
 const Users = Models.User; // Assign the User model to a constant for easy access
+const Directors = Models.Director; // Assign the Director model to a constant for easy access
+const Genres = Models.Genre; // Assign the Genre model to a constant for easy access
 
 const app = express();
 
@@ -31,23 +33,15 @@ app.get('/', (req, res) => {
 
 // GET /movies - Returns JSON data about all movies from the database
 app.get('/movies', (req, res) => {
-  console.log('Fetching all movies...'); // Log the action of fetching movies
-
-  Movies.find() // Query MongoDB for all movies
-    .then((movies) => {
-      console.log('Movies retrieved:', movies); // Log the retrieved movies
-      res.json(movies); // Respond with the movies in JSON format
-    })
-    .catch((err) => {
-      console.error('Error retrieving movies:', err); // Log any errors
-      res.status(500).json({ error: 'Error retrieving movies' }); // Respond with an error message
-    });
+  Movies.find()
+    .then((movies) => res.json(movies))
+    .catch((err) => res.status(500).json({ error: 'Error retrieving movies' }));
 });
 
 // GET /movies/:title - Returns data about a specific movie by title
 app.get('/movies/:title', (req, res) => {
   const { title } = req.params;
-  Movies.findOne({ Title: title }) // Match case with the schema
+  Movies.findOne({ title: { $regex: new RegExp('^' + title + '$', 'i') } })
     .then((movie) => {
       if (movie) {
         res.json(movie);
@@ -55,30 +49,88 @@ app.get('/movies/:title', (req, res) => {
         res.status(404).json({ error: 'Movie not found' });
       }
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ error: 'Error retrieving movie' });
-    });
+    .catch((err) => res.status(500).json({ error: 'Error retrieving movie' }));
 });
 
-// POST /users - Allows a new user to register
-app.post('/users', (req, res) => {
-  const { username, password, email, birthday } = req.body;
-  if (username && password && email && birthday) {
-    Users.create({
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday,
+// GET /users - Returns JSON data about all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(500).json({ error: 'Error retrieving users' }));
+});
+
+// GET /users/:username - Returns data about a specific user by username
+app.get('/users/:username', (req, res) => {
+  const { username } = req.params;
+  Users.findOne({ Username: username })
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
     })
-      .then((user) => res.json({ message: 'Registration successful', user }))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json({ error: 'Error registering user' });
-      });
-  } else {
-    res.status(400).json({ error: 'Please provide all required fields' });
-  }
+    .catch((err) => res.status(500).json({ error: 'Error retrieving user' }));
+});
+
+// DELETE /users/:username - Deletes a user by username
+app.delete('/users/:username', (req, res) => {
+  const { username } = req.params;
+  Users.findOneAndDelete({ Username: username })
+    .then((user) => {
+      if (user) {
+        res.json({ message: `User ${username} deleted successfully` });
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .catch((err) => res.status(500).json({ error: 'Error deleting user' }));
+});
+
+// GET /directors - Returns JSON data about all directors
+app.get('/directors', (req, res) => {
+  Directors.find()
+    .then((directors) => res.json(directors))
+    .catch((err) =>
+      res.status(500).json({ error: 'Error retrieving directors' })
+    );
+});
+
+// GET /directors/:name - Returns data about a specific director by name
+app.get('/directors/:name', (req, res) => {
+  const { name } = req.params;
+  Directors.findOne({ name: { $regex: new RegExp('^' + name + '$', 'i') } })
+    .then((director) => {
+      if (director) {
+        res.json(director);
+      } else {
+        res.status(404).json({ error: 'Director not found' });
+      }
+    })
+    .catch((err) =>
+      res.status(500).json({ error: 'Error retrieving director' })
+    );
+});
+
+// GET /genres - Returns JSON data about all genres
+app.get('/genres', (req, res) => {
+  Genres.find()
+    .then((genres) => res.json(genres))
+    .catch((err) => res.status(500).json({ error: 'Error retrieving genres' }));
+});
+
+// GET /genres/:name - Returns data about a specific genre by name
+app.get('/genres/:name', (req, res) => {
+  const { name } = req.params;
+  Genres.findOne({ name: { $regex: new RegExp('^' + name + '$', 'i') } })
+    .then((genre) => {
+      if (genre) {
+        res.json(genre);
+      } else {
+        res.status(404).json({ error: 'Genre not found' });
+      }
+    })
+    .catch((err) => res.status(500).json({ error: 'Error retrieving genre' }));
 });
 
 // Serve documentation.html file from the public folder
